9597c5c629509affd1e68ad8f59c52adeaf4d180
ramibaq <124310084+ramibaq@users.noreply.github.com>
2023-05-21 00:34:58 +0200

    Hope this is a complete application, more to do tomorrow

--- app/src/main/java/com/rami/firebasedemo2/MainActivity.java
+++ app/src/main/java/com/rami/firebasedemo2/MainActivity.java
@@ -10,6 +10,10 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
 
+    //First step; from pressing a button to a new activity!
+    private Button sellbtn;
+
+    private Button buybtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        //config - for button -> activity
+        sellbtn = (Button) findViewById(R.id.sellbtn);
+        sellbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSellActivity();
+            }
+                                   }
+
+        );
+
+        buybtn = (Button) findViewById(R.id.buybtn);
+        buybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBuyActivity();
+            }
+        });
+
+
+
+    }
+    //Last step for initializing sellbtn activity!
+    public void openSellActivity() {
+        Intent intent = new Intent(this, SellActivity.class);
+        startActivity(intent);
     }
 
+    public void openBuyActivity() {
+        Intent intent = new Intent(this, BuyActivity.class);
+        startActivity(intent);
+    }
 
 }
---
+++ app/src/main/java/com/rami/firebasedemo2/uploadinfo.java
@@ -0,0 +1,23 @@
+package com.rami.firebasedemo2;
+
+
+public class uploadinfo {
+
+    public String imageName;
+    public String imageURL;
+
+    public uploadinfo() {}
+
+    public uploadinfo(String name, String url) {
+        this.imageName = name;
+        this.imageURL = url;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+}
